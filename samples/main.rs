5fd373dd9833375813d21f292fdceec5ff864a9f
nicekingwei <nicekingwei@foxmail.com>
2019-05-22 19:31:14 +0800

    add sample

--- samples/main.rs
+++ samples/main.rs
@@ -1,23 +1,17 @@
 val x:i32 = 0;
-val y:i32 = 3+3*6;
+val y = fun(3+3*6);
 
-fn fun(x:i32) i32 {
+fn fun(x:i32) i8 {
     if x==0 {
         return 32;
+    } else if x==1 {
+        return 3;
     } else {
-        if x==1 {
-            return 3;
-        } else {
-            return 5;
-        }
+        return 5;
     }
 }
 
 fn main() unit {
     val y:i32 = 1;
     val z = fun(y);
-}
-
-fn __init() {
-    
 }
